026f15133c98f5fde4960ea69aeffebaae157f70
alejgh <alejandrgh11@gmail.com>
2018-03-12 16:17:24 +0100

    Last config modifications and testing that everything works fine.
    Signed-off-by: alejgh <alejandrgh11@gmail.com>

--- src/main/java/com/uniovi/main/InciDashboardI2bApplication.java
+++ src/main/java/com/uniovi/main/InciDashboardI2bApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.uniovi.main.InciManagerI2bApplication;
+import com.uniovi.main.InciDashboardI2bApplication;
 
 @SpringBootApplication
 @ComponentScan({
--- src/main/java/com/uniovi/services/KafkaConfig.java
+++ src/main/java/com/uniovi/services/KafkaConfig.java
@@ -1,10 +1,11 @@
-package com.uniovi.main;
+package com.uniovi.services;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -17,6 +18,9 @@ import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 @Configuration
 @EnableKafka
 public class KafkaConfig {
+	
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String bootstrapServers; // allows the project to be run inside a docker container
 	
     @Bean
     KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Integer, String>> kafkaListenerContainerFactory() {
@@ -36,13 +40,13 @@ public class KafkaConfig {
     @Bean
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "es.uniovi");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-consumer-group");
         return props;
     }
 }
