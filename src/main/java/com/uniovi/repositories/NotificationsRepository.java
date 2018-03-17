c7591a6289b7ea4729e5539f3ad05b34c95f940f
Pablo <pbalbuena13@gmail.com>
2018-03-18 00:42:49 +0100

    NotificationController created

--- src/main/java/com/uniovi/repositories/NotificationsRepository.java
+++ src/main/java/com/uniovi/repositories/NotificationsRepository.java
@@ -1,9 +1,13 @@
 package com.uniovi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.uniovi.entities.Notification;
 
-public interface NotificationRepository extends CrudRepository<Notification, Long>{
 
+public interface NotificationsRepository extends CrudRepository<Notification, Long>{
+
+	public List<Notification> findAll();
 }
