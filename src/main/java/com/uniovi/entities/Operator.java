8860ea7b5f86c1161748c4e98980d909794b866a
Pablo <pbalbuena13@gmail.com>
2018-03-18 00:22:00 +0100

    Mapping of Operator entity

--- src/main/java/com/uniovi/entities/Operator.java
+++ src/main/java/com/uniovi/entities/Operator.java
@@ -1,10 +1,17 @@
 package com.uniovi.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Operator {
@@ -14,37 +21,71 @@ public class Operator {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
 	
 	@Column(unique=true)
 	private String email;
 	
 	private String operatorname;
-	private int canRead;
-	private int canWrite;
-	private int canGrant;
+	private int isAdmin;
+	
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+    @JoinColumn(name="operator_id")
+	private Set<Notification> notifications = new HashSet<>();
 	
 	public Operator() {}
 	
-	/**
-	 * 
-	 * Constructor para la clase Operator
-	 * 
-	 * Permissions -> 1 if able, 0 otherwise
-	 * 
-	 * @param operatorname
-	 * @param canRead
-	 * @param canWrite
-	 * @param canGrant
-	 */
-	public Operator(String email, String operatorname, int canRead, int canWrite,
-			int canGrant) {
+	public Operator(Long id, String email, String operatorname, int isAdmin) {
 		super();
+		this.id = id;
 		this.email = email;
 		this.operatorname = operatorname;
-		this.canRead = canRead;
-		this.canWrite = canWrite;
-		this.canGrant = canGrant;
+		this.isAdmin = isAdmin;
 	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Operator other = (Operator) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Operator [email=" + email + ", operatorname=" + operatorname + ", isAdmin=" + isAdmin + "]";
+	}
+
+
 
 	public String getEmail() {
 		return email;
@@ -62,35 +103,23 @@ public class Operator {
 		this.operatorname = operatorname;
 	}
 
-	public int getCanRead() {
-		return canRead;
+	public int getIsAdmin() {
+		return isAdmin;
 	}
 
-	public void setCanRead(int canRead) {
-		this.canRead = canRead;
+	public void setIsAdmin(int isAdmin) {
+		this.isAdmin = isAdmin;
 	}
 
-	public int getCanWrite() {
-		return canWrite;
+	public Set<Notification> getNotifications() {
+		return notifications;
 	}
 
-	public void setCanWrite(int canWrite) {
-		this.canWrite = canWrite;
+	public void setNotifications(Set<Notification> notifications) {
+		this.notifications = notifications;
 	}
 
-	public int getCanGrant() {
-		return canGrant;
-	}
 
-	public void setCanGrant(int canGrant) {
-		this.canGrant = canGrant;
-	}
-
-	@Override
-	public String toString() {
-		return "Operator [operatorname=" + operatorname + ", canRead=" + canRead
-				+ ", canWrite=" + canWrite + ", canGrant=" + canGrant + "]";
-	}
 	
 	
 }
