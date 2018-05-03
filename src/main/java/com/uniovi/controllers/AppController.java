2b4251fc265ff96730fff2fc6c5a88f6cb2a2677
SergioFaya <estisergio@gmail.com>
2018-05-03 18:10:54 +0200

    Created a superclass for dashboard controllers. This controller contains basic fields that must be passed to all templates. This is basicalli for the websockets

---
+++ src/main/java/com/uniovi/controllers/AppController.java
@@ -0,0 +1,60 @@
+package com.uniovi.controllers;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+
+import com.uniovi.entities.Operator;
+import com.uniovi.services.IncidentsService;
+import com.uniovi.services.OperatorsService;
+
+public class AppController {
+
+	@Autowired
+	protected OperatorsService operatorsService;
+
+	@Autowired
+	protected IncidentsService incidentsService;
+
+	
+	public AppController() {
+		super();
+	}
+
+	protected void addCommonAttributes(Model model, Principal principal) {
+		Operator o = operatorsService.getOperatorByEmail(principal.getName());
+		model.addAttribute("opEmail", SecurityContextHolder.getContext().getAuthentication().getName());
+		model.addAttribute("numNotifications", this.getNotificationsOfCurrentOp());
+		model.addAttribute("numIncidents", this.getIncidencesOfCurrentOp());
+		model.addAttribute("role", o.getRole());
+		model.addAttribute("incidentModify", o.isIncidentModify());
+		model.addAttribute("chartAccess", o.isChartAccess());
+		model.addAttribute("mapAccess", o.isMapAccess());
+	}
+	
+	protected void operatorPermissions(Model model , Operator o) {
+		model.addAttribute("incidentModify", o.isIncidentModify());
+		model.addAttribute("chartAccess", o.isChartAccess());
+		model.addAttribute("mapAccess", o.isMapAccess());
+	}
+	
+	protected int getNotificationsOfCurrentOp() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String email = authentication.getName();
+		Operator operator = operatorsService.getOperatorByEmail(email);
+		return operator.getNumNotifications();
+	}
+	
+	protected int getIncidencesOfCurrentOp() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String email = authentication.getName();
+		Operator operator = operatorsService.getOperatorByEmail(email);
+		return incidentsService.getIncidentsOf(operator).size();
+	}
+
+	
+	
+}
--- src/main/java/com/uniovi/controllers/DashboardController.java
+++ src/main/java/com/uniovi/controllers/DashboardController.java
@@ -6,8 +6,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,22 +13,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.uniovi.entities.Incident;
-import com.uniovi.entities.Operator;
-import com.uniovi.services.IncidentsService;
 import com.uniovi.services.InsertTestDataService;
-import com.uniovi.services.OperatorsService;
 
 @Controller
-public class DashboardController {
+public class DashboardController extends AppController {
 
 	@Autowired
 	private InsertTestDataService testDataService;
-
-	@Autowired
-	private IncidentsService inciService;
-
-	@Autowired
-	private OperatorsService operatorsService;
 
 	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
 	public String getDashboard() {
@@ -62,40 +51,13 @@ public class DashboardController {
 	public String getTestData(Model model) throws JsonProcessingException {
 		return testDataService.getTestDataAsJSON();
 	}
-
-	private int getNotificationsOfCurrentOp() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String email = authentication.getName();
-		Operator operator = operatorsService.getOperatorByEmail(email);
-		return operator.getNumNotifications();
-	}
-
-	private int getIncidencesOfCurrentOp() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String email = authentication.getName();
-		Operator operator = operatorsService.getOperatorByEmail(email);
-		return inciService.getIncidentsOf(operator).size();
-	}
-	/*
-	 * Duplicated in the other controller. Not pretty but working, should create an super controller or something
-	 */
-	private void addCommonAttributes(Model model, Principal principal) {
-		Operator o = operatorsService.getOperatorByEmail(principal.getName());
-		model.addAttribute("opEmail", SecurityContextHolder.getContext().getAuthentication().getName());
-		model.addAttribute("numNotifications", this.getNotificationsOfCurrentOp());
-		model.addAttribute("numIncidents", this.getIncidencesOfCurrentOp());
-		model.addAttribute("role", o.getRole());
-		model.addAttribute("incidentModify", o.isIncidentModify());
-		model.addAttribute("chartAccess", o.isChartAccess());
-		model.addAttribute("mapAccess", o.isMapAccess());
-	}
-
+	
 	private void addChartsAttributes(Model model) {
-		List<Incident> temperatureIncidents = inciService.getTemperatureSensorIncidents();
-		Map<String, Integer> usedTags = inciService.getMostUsedTags();
-		List<Incident> incidentsSensors = inciService.getKindIncidents("Sensor");
-		List<Incident> incidentsPeople = inciService.getKindIncidents("Person");
-		List<Incident> incidentsEntities = inciService.getKindIncidents("Entity");
+		List<Incident> temperatureIncidents = incidentsService.getTemperatureSensorIncidents();
+		Map<String, Integer> usedTags = incidentsService.getMostUsedTags();
+		List<Incident> incidentsSensors = incidentsService.getKindIncidents("Sensor");
+		List<Incident> incidentsPeople = incidentsService.getKindIncidents("Person");
+		List<Incident> incidentsEntities = incidentsService.getKindIncidents("Entity");
 		model.addAttribute("sensors", incidentsSensors.size());
 		model.addAttribute("people", incidentsPeople.size());
 		model.addAttribute("entities", incidentsEntities.size());
@@ -105,16 +67,16 @@ public class DashboardController {
 	}
 
 	private void addRealTimeAttributes(Model model) {
-		List<Incident> incidentsSensors = inciService.getKindIncidents("Sensor");
-		List<Incident> incidentsPeople = inciService.getKindIncidents("Person");
-		List<Incident> incidentsEntities = inciService.getKindIncidents("Entity");
+		List<Incident> incidentsSensors = incidentsService.getKindIncidents("Sensor");
+		List<Incident> incidentsPeople = incidentsService.getKindIncidents("Person");
+		List<Incident> incidentsEntities = incidentsService.getKindIncidents("Entity");
 		model.addAttribute("sensors", incidentsSensors);
 		model.addAttribute("people", incidentsPeople);
 		model.addAttribute("entities", incidentsEntities);
 	}
 
 	private void addMapAttributes(Model model) {
-		List<Incident> incidents = inciService.getGeolocatedIncidents();
+		List<Incident> incidents = incidentsService.getGeolocatedIncidents();
 		model.addAttribute("incidents", incidents);
 	}
 
