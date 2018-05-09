c6d23bddd9825a04318ce5d7aedecbcdb3c6d154
Pablo <pbalbuena13@gmail.com>
2018-05-09 19:18:13 +0200

    Chrome driver deleted

--- src/test/java/es/uniovi/asw/CucumberTest.java
+++ src/test/java/es/uniovi/asw/CucumberTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features")
 public class CucumberTest {
 
